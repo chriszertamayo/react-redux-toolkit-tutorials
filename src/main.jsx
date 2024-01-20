a2d7630dd98a814bfa8dcd59e5dd5ff1235d9099
chriszer tamayo <chriszertamayo@chriszers-MacBook-Pro.local>
2024-01-21 00:58:13 +0800

    with-redux-thunk-middleware section

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {store } from './app/store.jsx'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./app/store.jsx";
+import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+store.dispatch(fetchUsers());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
-)
+  </Provider>
+  // </React.StrictMode>
+);
