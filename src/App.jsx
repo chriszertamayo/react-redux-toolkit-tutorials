d2899f89521b1c3be401681e566d3e78383d34bf
chriszer tamayo <chriszertamayo@chriszers-MacBook-Pro.local>
2024-02-06 14:43:25 +0800

    done optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-// import Counter from './features/counter/Counter'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import EditPostForm from "./features/posts/EditPostForm";
 import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
-// import './App.css'
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -20,12 +18,16 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/*Catch all - replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to={"/"} replace />} />
       </Route>
     </Routes>
-    // <main className="App">
-    //   <AddPostForm />
-    //   <PostsList />
-    // </main>
   );
 }
 
