d380fbac15ea28dfabab5ded9a604262482149d2
chriszer tamayo <chriszertamayo@chriszers-MacBook-Pro.local>
2024-02-08 23:44:26 +0800

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { store } from "./app/store.jsx";
-import { Provider } from "react-redux";
-import { fetchPosts } from "./features/posts/postsSlice.jsx";
-import { fetchUsers } from "./features/users/usersSlice.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
 );
